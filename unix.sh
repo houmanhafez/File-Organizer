4a0030e2f97202ee18873d4d6755961f9dddd059
Houman Hafez Alghoran <houman.hafezalghoran@gmail.com>
2024-06-10 23:54:25 +0200

    Revert "updated the shell scripts to work in the background and added dependencies"
    This reverts commit a58efb470b8e8a6572b341f9eeda19f63393d391.

--- unix.sh
+++ unix.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 
-nohup python3 /path/to/your/script.py > /dev/null 2>&1 &
+source venv/bin/activate
+
+python script.py &
+
+PYTHON_PID=$!
+
+read -p "Enter your browser (chrome, edge, firefox, brave, vivaldi, tor, chromium, arc): " browser
+
+case $browser in
+chrome)
+    open -a "Google Chrome"
+    ;;
+edge)
+    open -a "Microsoft Edge"
+    ;;
+firefox)
+    open -a "Firefox"
+    ;;
+brave)
+    open -a "Brave Browser"
+    ;;
+vivaldi)
+    open -a "Vivaldi"
+    ;;
+tor)
+    open -a "Tor Browser"
+    ;;
+chromium)
+    open -a "Chromium"
+    ;;
+arc)
+    open -a "Arc"
+    ;;
+*)
+    echo "Browser not recognized. Please enter one of the specified browsers."
+    ;;
+esac
+
+wait $PYTHON_PID
