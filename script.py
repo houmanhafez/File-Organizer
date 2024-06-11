2c9af788241c5abd054799e2e8d457605f6fdc5c
Houman Hafez <houman.hafezalghoran@gmail.com>
2024-06-12 00:46:39 +0200

    Script Monitoring (#4)
    * Updated the scripts to work in the background. Added requirements.txt
    
    * Added functionalities such as working only when a browser in the list is detected and observing desktop directory

--- script.py
+++ script.py
@@ -5,22 +5,25 @@ from pathlib import Path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import time
+import psutil
+
 
 class FileOrganizerHandler(FileSystemEventHandler):
-    def __init__(self, folder_to_track):
-        self.folder_to_track = folder_to_track
+    def __init__(self, folders_to_track):
+        self.folders_to_track = folders_to_track
 
     def on_modified(self, event):
         """
-        Callback method triggered when a modification event is detected in the monitored folder.
-        
+        Callback method triggered when a modification event is detected in the monitored folders.
+
         Args:
             event (FileSystemEvent): The event parameter passed by the watchdog library.
         """
-        for filename in os.listdir(self.folder_to_track):
-            src = os.path.join(self.folder_to_track, filename)
-            if os.path.isfile(src):
-                self.organize_file(src)
+        for folder_to_track in self.folders_to_track:
+            for filename in os.listdir(folder_to_track):
+                src = os.path.join(folder_to_track, filename)
+                if os.path.isfile(src):
+                    self.organize_file(src)
 
     def organize_file(self, src):
         mime_type, _ = mimetypes.guess_type(src)
@@ -31,7 +34,7 @@ class FileOrganizerHandler(FileSystemEventHandler):
                 self.move_file(src, "Images")
             elif "html" in mime_type:
                 self.move_file(src, "HTMLs")
-            elif "executables" in mime_type:
+            elif "executable" in mime_type:
                 self.move_file(src, "Executables")
             else:
                 self.move_file(src, "Others")
@@ -39,25 +42,35 @@ class FileOrganizerHandler(FileSystemEventHandler):
             self.move_file(src, "Others")
 
     def move_file(self, src, folder_name):
-        dest_folder = os.path.join(self.folder_to_track, folder_name)
+        downloads_folder = get_downloads_folder()
+        dest_folder = os.path.join(downloads_folder, folder_name)
         if not os.path.exists(dest_folder):
             os.makedirs(dest_folder)
         shutil.move(src, os.path.join(dest_folder, os.path.basename(src)))
 
+
 def get_downloads_folder():
     home = str(Path.home())
-    if os.name == 'nt':  
-    # Windows
-        downloads_folder = os.path.join(home, 'Downloads')
-    # macOS/Linux
-    else:  
-        downloads_folder = os.path.join(home, 'Downloads')
-    return downloads_folder
+    return os.path.join(home, "Downloads")
 
-def start_observer(folder_to_track):
-    event_handler = FileOrganizerHandler(folder_to_track)
+
+def get_desktop_folder():
+    home = str(Path.home())
+    onedrive_path = os.path.join(home, "OneDrive", "Desktop")
+    if os.path.exists(onedrive_path):
+        return onedrive_path
+    return os.path.join(home, "Desktop")
+
+
+def start_observer(folders_to_track):
+    for folder in folders_to_track:
+        if not os.path.exists(folder):
+            raise FileNotFoundError(f"The folder {folder} does not exist.")
+
+    event_handler = FileOrganizerHandler(folders_to_track)
     observer = Observer()
-    observer.schedule(event_handler, folder_to_track, recursive=False)
+    for folder in folders_to_track:
+        observer.schedule(event_handler, folder, recursive=False)
     observer.start()
     try:
         while True:
@@ -66,7 +79,38 @@ def start_observer(folder_to_track):
         observer.stop()
     observer.join()
 
+
+def is_browser_open():
+    browser_processes = [
+        "chrome.exe",
+        "firefox.exe",
+        "msedge.exe",
+        "safari.exe",
+        "Arc.exe",
+    ]  # Add other browser executables as needed
+    for process in psutil.process_iter(["name"]):
+        if process.info["name"] in browser_processes:
+            return True
+    return False
+
+
+def monitor_browsers_and_start():
+    already_running = False
+    while True:
+        if is_browser_open():
+            if not already_running:
+                print("Browser detected. Starting file organizer.")
+                folders_to_track = [get_downloads_folder(), get_desktop_folder()]
+                print(f"Monitoring folders: {folders_to_track}")
+                start_observer(folders_to_track)
+                already_running = True
+        else:
+            if already_running:
+                print("No browser detected. Stopping file organizer.")
+                # Handle stopping the observer if needed
+                already_running = False
+        time.sleep(5)
+
+
 if __name__ == "__main__":
-    folder_to_track = get_downloads_folder()
-    print(f"Monitoring folder: {folder_to_track}")
-    start_observer(folder_to_track)
+    monitor_browsers_and_start()
